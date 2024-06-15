c1b5b7cf6b2b4cd4975611eec9e9142ea4d73895
Eric Moore <ericmoore0709@gmail.com>
2024-06-14 19:04:45 -0700

    initial app created

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: "Success!" });
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+})
